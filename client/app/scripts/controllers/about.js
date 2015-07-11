1a0ced745568ca23a1ab0310df00e7057af84e42
jahde <jahde.hiywan@gmail.com>
2015-07-11 14:13:48 -0400

    grunt up and running

---
+++ client/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gymBuddyApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gymBuddyApp
+ */
+angular.module('gymBuddyApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ client/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gymBuddyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gymBuddyApp
+ */
+angular.module('gymBuddyApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
